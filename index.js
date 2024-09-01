5b3f6e82de192a69645c08c003d8e63c94657e46
cassadycampos <cassady801@gmail.com>
2024-08-31 21:46:17 -0600

    WROTE FUNCTION TO UPLOAD BACK TO GDRIVE!!!

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
+const Readable = require('stream').Readable;
+
+
 const lambda = new AWS.Lambda();
 const ssm = new AWS.SSM();
 
 const {JWT} = require('google-auth-library');
 const {google} = require('googleapis');
 const { file } = require('googleapis/build/src/apis/file');
+const { GetObjectCommand, S3Client } = require('@aws-sdk/client-s3')
 
 async function authorize() {
   const data = await ssm.getParameter({
@@ -35,14 +40,18 @@ exports.handler = async (event) => {
 
     const result = await runTransformationLambda(event);
 
-    const payload = JSON.parse(result.Payload);
-    const body = JSON.parse(payload.body);
-    console.log(body.message);
+    console.log("Starting uploadFromS3ToDrive...");
+    await uploadFromS3ToDrive(authClient);
 
+    // const payload = JSON.parse(result.Payload);
+    // const body = JSON.parse(payload.body);
+    // console.log(body.message);
+
+    
     return {
       statusCode: 200,
-      body: "Success: " + body.message
-      // body: "Success: done "
+      // body: "Success: " + body.message
+      body: "Success: done "
     }
   }
   catch (error) {
@@ -50,6 +59,80 @@ exports.handler = async (event) => {
     return {
       statusCode: 500,
       body: JSON.stringify({ mesage: "An error occured during the run: " + error.message})
+    };
+  }
+}
+
+async function uploadFromS3ToDrive(authClient) {
+  try {
+    const input = {
+      Bucket: "mint-transactions",
+      Prefix: "transformed/",
+      "MaxKeys": "12"
+    };
+    const s3Objects = await s3.listObjectsV2(input).promise();
+    
+    console.log("Listed objects successfully. Entering promise statement.");
+    console.log(JSON.stringify(s3Objects, null, 2)); // Pretty-print with indentation
+    
+    
+    const client = new S3Client() // Pass in opts to S3 if necessary
+
+    await Promise.all(s3Objects.Contents.map(async (object) => {
+        console.log("Attempting to grab object...");
+        console.log("Key: " + object.Key);
+        if (!object.Key.endsWith(".xlsx")) {
+          return;
+        }
+
+        const input = {
+          "Bucket": "mint-transactions",
+          "Key": object.Key
+        };
+        const command = new GetObjectCommand(input);
+        const response = await client.send(command);
+
+        console.log("response: " + response.Body);
+        console.log("ResponseReadable: " + Readable.from(response.Body));
+
+        const csv_content = Readable.from(response.Body);
+        // const csv_content = response.Body.toString("utf8");
+        // console.log("ContentsCassady: " + csv_content.);
+
+        // upload now
+        console.log("Attempting to upload to drive...");
+        const drive = google.drive({version: 'v3', auth: authClient});
+        
+        const folderId = process.env.DESTINATION_FOLDER_ID
+        
+        const requestBody = {
+          name: object.Key,
+          parents: [folderId],
+        };
+
+        const media = {
+          mimeType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+          body: csv_content,
+        };
+
+        const file = await drive.files.create({
+          requestBody: requestBody,
+          media: media,
+          fields: 'id',
+        });
+
+        console.log('Upload success, file Id:', file.data.id);
+        return file.data.id;
+    }));
+
+  }
+  catch (error) {
+    console.error("An error occured in the upload to drive function:", error);
+    throw error;
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "An error occured in the upload to drive function:", error})
     };
   }
 }
